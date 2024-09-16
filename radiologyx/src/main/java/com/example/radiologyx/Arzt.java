1d56de2e1d782577b4e1966f6fe352cce3bcb269
Semi21206 <155948698+Semi21206@users.noreply.github.com>
2024-09-16 13:32:14 +0200

    Delete radiologyx directory
    Signed-off-by: Semi21206 <155948698+Semi21206@users.noreply.github.com>

--- radiologyx/src/main/java/com/example/radiologyx/Arzt.java
+++
@@ -1,6 +0,0 @@
-package com.example.radiologyx;
-
-public class Arzt {
-
-
-}
--- radiologyx/src/main/java/com/example/radiologyx/UserInterface.java
+++
@@ -1,9 +0,0 @@
-package com.example.radiologyx;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserInterface extends JpaRepository<User, Long> {
-
-}
