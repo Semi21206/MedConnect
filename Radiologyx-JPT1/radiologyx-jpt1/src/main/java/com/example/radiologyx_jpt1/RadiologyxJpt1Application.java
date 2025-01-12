61c2cc0d2e34dffc45f15101e13f99c4431ec12e
Zakir Barang <bar210468@spengergasse.at>
2025-01-12 03:34:41 +0100

    09.01.2024 fast fertige version noch etwas userinterface fehlt und das wars

--- Radiologyx-JPT1/radiologyx-jpt1/src/main/java/com/example/radiologyx_jpt1/RadiologyxJpt1Application.java
+++ Radiologyx-JPT1/radiologyx-jpt1/src/main/java/com/example/radiologyx_jpt1/RadiologyxJpt1Application.java
@@ -2,11 +2,13 @@ package com.example.radiologyx_jpt1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example.radiologyx_jpt1")
 public class RadiologyxJpt1Application {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(RadiologyxJpt1Application.class, args);
 	}
 
